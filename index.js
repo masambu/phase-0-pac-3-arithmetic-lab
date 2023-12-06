a0cf014f7c5c9e88cc296cb4fc63bc86ac842d27
Kelvin <kevohmasambu@gmail.com>
2023-12-06 16:48:46 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,56 @@
+function add(a, b) {
+    return (a + b)
+}
+console.log(add(2, 3));
+
+
+
+function subtract(a, b) {
+    return (a - b)
+}
+console.log(subtract(2, 3));
+
+
+
+function multiply(a, b) {
+    return (a * b)
+}
+console.log(multiply(2, 3));
+
+
+
+function divide(a, b) {
+    return (a / b)
+}
+console.log(divide(2, 3));
+
+
+
+function increment(n) {
+    return n + 1;
+}
+console.log(increment(5));
+
+
+
+
+function decrement(n) {
+    return n - 1;
+}
+console.log(decrement(5));
+
+
+
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+console.log(makeInt('mandunya'));
+
+
+
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+console.log(preserveDecimal("2.246746792"));
